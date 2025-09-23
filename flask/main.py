e4dca04d12a2346da95cf19f1fd956f7097dc932
brian <lbc.brian.chilin@gmail.com>
2025-09-22 17:09:31 -0700

    working index and flask

--- flask/main.py
+++ flask/main.py
@@ -1,8 +1,8 @@
 # import sys # useful for debug
 from pathlib import Path
 import time
-from flask import Flask
-from argon2 import PasswordHasher
+from flask import Flask, request
+from argon2 import PasswordHasher, exceptions
 import psycopg2.pool
 import psycopg2
 
@@ -18,26 +18,61 @@ pool = psycopg2.pool.SimpleConnectionPool(
 	database=(config_path / "db_name").read_text().strip()
 )
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
 	#print("index route entered", file=sys.stderr, flush=True) #debug
 	start = time.perf_counter()
 	conn = pool.getconn()
+	raw_html = f"<p>Current time is {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}</p>"
 	try:
 		with conn.cursor() as cur:
 			cur.execute("SELECT * FROM users ORDER BY id;")
-			s = "<table>"
+			raw_html += "<table><tr><th>id</th><th>username</th><th>password</th><th>raw_password</th></tr>"
 			for a, b, c, d in cur.fetchall():
-				s += "<tr>"
-				s += "<td>" + str(a) + "</td>"
-				s += "<td>" + str(b) + "</td>"
-				s += "<td>" + str(c) + "</td>"
-				s += "<td>" + str(d if d else "NULL") + "</td>"
-				s += "</tr>"
-			s+="</table>"
-			return f"Current time is {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())} <br>{s} <br><br>{(time.perf_counter()-start):.3f}s"
+				raw_html += "<tr>"
+				raw_html += "<td>" + str(a) + "</td>"
+				raw_html += "<td>" + str(b) + "</td>"
+				raw_html += "<td>" + str(c) + "</td>"
+				raw_html += "<td>" + str(d if d else "NULL") + "</td>"
+				raw_html += "</tr>"
+
+			request_result, username_value = "", ""
+			if request.method == "POST":
+				username_value = request.form['username']
+				cur.execute(
+					"SELECT id, username, password FROM users WHERE username=%s;",
+					(request.form["username"],)
+				)
+				row = cur.fetchone()
+				request_result = "❌ invalid credentials" #if not row --> username was wrong
+				if row:
+					try:
+						ph.verify(row[2], request.form["password"])
+						request_result = "✅ credentials verified"
+					except exceptions.VerifyMismatchError:
+						request_result = "❌ invalid credentials" #password was wrong
+					except Exception as e:
+						request_result = "server error"
+
+			raw_html += "</table>"
+			raw_html += f"""
+				<form method="post">
+					<table>
+						<tr>
+							<td><label>Username:</label></td>
+							<td><input type="text" name="username" value="{username_value}" required></td>
+						</tr><tr>
+							<td><label>Password:</label></td>
+							<td><input type="password" name="password" required></td>
+						</tr>
+					</table>
+					<button type="submit">Verify</button>{request_result}
+				</form>
+			"""
+			raw_html += f"<p>{(time.perf_counter()-start):.3f}s from Flask</p>"
+			return raw_html
 	except Exception as e:
-		return f"server error:<br>{e} <br><br>{(time.perf_counter()-start):.3f}s", 500
+		return f"<p>server error:<p></p>{e}</p> <p>{(time.perf_counter()-start):.3f}s from Flask</p>", 500
 	finally:
 		pool.putconn(conn)
 
--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
-### simple login systems that all share one database
+### simple hash verification systems that all share one database
 - only reading is supported.
 - these projects are simple enough i opted to work without dev containers
 - index is special because it's static
 
-TODO: REFERENCE K8S DEPLOYMENT
+TODO: REFERENCE K8S DEPLOYMENT  
 TODO: document build/deployment process. maybe automate some stuff
 
 #### index
