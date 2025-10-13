ddb5844b6819f13bfd154e17c51f98ced7e227c2
brian <lbc.brian.chilin@gmail.com>
2025-10-12 19:08:55 -0700

    seemingly correctly reading post request form data (might be failing on null stuff)

--- aspnet/aspnetProject/Program.cs
+++ aspnet/aspnetProject/Program.cs
@@ -3,6 +3,8 @@ using Npgsql;
 using Stubble.Core.Interfaces;
 using Stubble.Core.Builders;
 
+using System; //debug
+
 var builder = WebApplication.CreateBuilder(args);
 
 string html_template_str = File.ReadAllText("/etc/config/html/html_template").Trim();
@@ -24,13 +26,25 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapMethods("/", new[] { "GET", "POST" }, async (NpgsqlDataSource db, IStubbleRenderer mustache) => {
+app.MapMethods("/", new[] { "GET", "POST" }, async (HttpRequest request, NpgsqlDataSource db, IStubbleRenderer mustache) => {
     await using var conn = await db.OpenConnectionAsync();
     await using var cmd = new NpgsqlCommand("SELECT NOW()", conn);
     var serverTime = await cmd.ExecuteScalarAsync();
 
+
+
+    string form_username = "";
+    string form_password = "";
+    if (HttpMethods.IsPost(request.Method)) {
+        var form = await request.ReadFormAsync();
+        form_username = form["username"];
+        form_password = form["password"];
+        Console.WriteLine($"post password:{form_password}");
+    }
+
     var model = new {
-        STATUS = "aspnet"
+        STATUS = "aspnet",
+        FORM_USERNAME_VALUE = form_username,
     };
 
     return Results.Content(
